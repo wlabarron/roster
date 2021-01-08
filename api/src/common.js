b7694d76f4cb1ba7a7876371b5fcb05ca2554374
Andrew Barron <hello@awmb.uk>
2021-01-08 15:04:48 +0000

    Field validating function and generator

--- api/src/common.js
+++ api/src/common.js
@@ -17,4 +17,42 @@ function validateUUID(uuid) {
     return uuid === "all" || /^[0-9]{17}$/.test(uuid);
 }
 
-module.exports = {validateUUID};
+/**
+ * Creates a function to check if a given array of fields matches an approved set of field names.
+ *
+ * The returned function takes one parameter, an array of fields. Values which are not on the valid list are removed
+ * (checked using a case-insensitive match with whitespace trimmed), and a new array of normalised field names in the
+ * same order as the list of valid field names is returned. If nothing valid is found, an empty array is returned.
+ * @param {Array<string>} validFieldNames The array of field names which are valid, in the order and case you expect
+ * the array to check to be normalised to.
+ * @returns {function(fields:Array<string>)} A validator function based on the set of valid field names.
+ */
+function createFieldValidator(validFieldNames) {
+    return function validateFields(fields) {
+        // Check fields are of correct type and not empty.
+        if (Object.prototype.toString.call(fields) !== "[object Array]" ||
+            fields.length < 1) {
+            return [];
+        }
+
+        // Normalise fields for comparing
+        let normalisedFields = fields.map(value => {
+            return value.trim().toLowerCase();
+        });
+
+        let finalisedFields = [];
+
+        // Work through all of the valid field names, checking if the set of fields requested includes that specific
+        // valid field name. If it does, add the field name to the array of finalised fields. Otherwise, ignore it and
+        // move on.
+        validFieldNames.forEach((validFieldName) => {
+            if (normalisedFields.includes(validFieldName.toLowerCase())) {
+                finalisedFields.push(validFieldName);
+            }
+        })
+
+        return finalisedFields;
+    }
+}
+
+module.exports = {validateUUID, createFieldValidator};
--- api/tests/common.test.js
+++ api/tests/common.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const common = require('../src/common');
+const validateFields = common.createFieldValidator(["nick", "name", "description", "email", "profileImage", "coverImage", "url"]);
 
 test('Check if "all" is treated as a valid UUID', () => {
     expect(common.validateUUID("all")).toBe(true);
@@ -43,3 +44,71 @@ test('Check if various invalid UUIDs are rejected correctly', () => {
     expect(common.validateUUID("help me I'm stuck in a test writing factory and can't get out")).toBe(false);
     expect(common.validateUUID("twoHundredAndFortySeven")).toBe(false);
 });
+
+
+test('Check various valid sets of fields to request are handled correctly', () => {
+    // Map, mapping inputs to expected outputs
+    let sets = new Map();
+    sets.set(["nick"],
+        ["nick"]);
+    sets.set(["nick", "name"],
+        ["nick", "name"]);
+    sets.set(["nick", "name", "description"],
+        ["nick", "name", "description"]);
+    sets.set(["nick", "name", "description", "email"],
+        ["nick", "name", "description", "email"]);
+    sets.set(["nick", "name", "description", "email", "profileImage"],
+        ["nick", "name", "description", "email", "profileImage"]);
+    sets.set(["nick", "name", "description", "email", "profileImage", "coverImage"],
+        ["nick", "name", "description", "email", "profileImage", "coverImage"]);
+    sets.set(["nick", "name", "description", "email", "profileImage", "coverImage", "url"],
+        ["nick", "name", "description", "email", "profileImage", "coverImage", "url"]);
+    sets.set(["nick", "name", "email", "profileImage", "url"],
+        ["nick", "name", "email", "profileImage", "url"]);
+    sets.set(["nick", "name", "description", "email", "profileImage", "coverImage", "url"],
+        ["nick", "name", "description", "email", "profileImage", "coverImage", "url"]);
+    sets.set(["name", "description", "profileImage", "coverImage", "url"],
+        ["name", "description", "profileImage", "coverImage", "url"]);
+    sets.set(["description", "email", "url"],
+        ["description", "email", "url"]);
+
+    for (const input of sets.keys()) {
+        // Expect the set to be treated as valid and return correctly
+        expect(validateFields(input)).toStrictEqual(sets.get(input));
+    }
+})
+
+test('Check various valid sets of fields which are incorrectly ordered are reordered and treated as valid', () => {
+    // Map, mapping inputs to expected outputs
+    let sets = new Map();
+    sets.set(["name", "nick"],
+        ["nick", "name"]);
+    sets.set(["url", "coverImage", "profileImage", "email", "description", "name", "nick"],
+        ["nick", "name", "description", "email", "profileImage", "coverImage", "url"]);
+
+    for (const input of sets.keys()) {
+        // Expect the set to be treated as valid and return correctly
+        expect(validateFields(input)).toStrictEqual(sets.get(input));
+    }
+})
+
+test('Check invalid fields are removed during validation', () => {
+    // Map, mapping inputs to expected outputs
+    let sets = new Map();
+    sets.set(["nick", "frank"],
+        ["nick"]);
+    sets.set(["nick", "help me I'm stuck in a test factory", "name"],
+        ["nick", "name"]);
+    sets.set(["I can't tell what's JavaScript and real life any more", "nick", "name", "description"],
+        ["nick", "name", "description"]);
+
+    for (const input of sets.keys()) {
+        // Expect the set to be treated as valid and return correctly
+        expect(validateFields(input)).toStrictEqual(sets.get(input));
+    }
+})
+
+test('Check various types of emptiness are treated correctly', () => {
+    expect(validateFields([])).toStrictEqual([]);
+    expect(validateFields("")).toStrictEqual([]);
+})
