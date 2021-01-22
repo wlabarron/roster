bc6a96bcd85d8201600bbe0a1b66b21bb476392c
Andrew Barron <hello@awmb.uk>
2021-01-22 15:29:09 +0000

    Update database schema as design changed

--- schema.sql
+++ schema.sql
@@ -1,21 +1,21 @@
 create table images
 (
-    id  bigint unsigned default(uuid_short())
+    id  bigint unsigned default uuid_short() not null
         primary key,
-    url text not null,
-    alt text not null
+    url text                                 not null,
+    alt text                                 not null
 );
 
 create table people
 (
-    id           bigint unsigned default(uuid_short())
+    id           bigint unsigned default uuid_short() not null
         primary key,
-    nick         text not null,
-    name         text not null,
-    description  text null,
-    email        text null,
-    profileImage bigint unsigned  null,
-    coverImage   bigint unsigned null,
+    nick         text                                 not null,
+    name         text                                 not null,
+    description  text                                 null,
+    email        text                                 null,
+    profileImage bigint unsigned                      null,
+    coverImage   bigint unsigned                      null,
     constraint people_coverImage
         foreign key (coverImage) references images (id),
     constraint people_profileImage
@@ -24,15 +24,15 @@ create table people
 
 create table shows
 (
-    id           bigint unsigned default(uuid_short())
+    id           bigint unsigned default uuid_short() not null
         primary key,
-    nick         text not null,
-    name         text not null,
-    tagline      text null,
-    description  text null,
-    email        text null,
-    profileImage bigint unsigned null,
-    coverImage   bigint unsigned null,
+    nick         text                                 not null,
+    name         text                                 not null,
+    tagline      text                                 null,
+    description  text                                 null,
+    email        text                                 null,
+    profileImage bigint unsigned                      null,
+    coverImage   bigint unsigned                      null,
     constraint shows_coverImage
         foreign key (coverImage) references images (id),
     constraint shows_profileImage
@@ -41,11 +41,11 @@ create table shows
 
 create table rel_shows_people
 (
-    id     bigint unsigned default(uuid_short())
+    id     bigint unsigned default uuid_short() not null
         primary key,
-    `show` bigint unsigned not null,
-    person bigint unsigned not null,
-    role   text null,
+    `show` bigint unsigned                      not null,
+    person bigint unsigned                      not null,
+    role   text                                 null,
     constraint rel_shows_people_person
         foreign key (person) references people (id),
     constraint rel_shows_people_shows
@@ -54,14 +54,14 @@ create table rel_shows_people
 
 create table sponsors
 (
-    id           bigint unsigned default(uuid_short())
+    id           bigint unsigned default uuid_short() not null
         primary key,
-    nick         text not null,
-    name         text not null,
-    description  text not null,
-    email        text null,
-    profileImage bigint unsigned null,
-    coverImage   bigint unsigned null,
+    nick         text                                 not null,
+    name         text                                 not null,
+    description  text                                 not null,
+    email        text                                 null,
+    profileImage bigint unsigned                      null,
+    coverImage   bigint unsigned                      null,
     constraint sponsors_coverImage
         foreign key (coverImage) references images (id),
     constraint sponsors_profileImage
@@ -70,11 +70,11 @@ create table sponsors
 
 create table rel_shows_sponsors
 (
-    id      bigint unsigned default(uuid_short())
+    id      bigint unsigned default uuid_short() not null
         primary key,
-    `show`  bigint unsigned not null,
-    sponsor bigint unsigned not null,
-    detail  text null,
+    `show`  bigint unsigned                      not null,
+    sponsor bigint unsigned                      not null,
+    detail  text                                 null,
     constraint rel_shows_sponsors_show
         foreign key (`show`) references shows (id),
     constraint rel_shows_sponsors_sponsor
@@ -83,34 +83,36 @@ create table rel_shows_sponsors
 
 create table times
 (
-    id         bigint unsigned default(uuid_short())
+    id                bigint unsigned default uuid_short() not null
         primary key,
-    `show`     bigint unsigned      not null,
-    new        tinyint(1) default 1 not null,
-    datetime   text                 not null,
-    exceptions text                 null,
-    start      time                 not null,
-    end        time                 not null,
+    `show`            bigint unsigned                      not null,
+    new_show          tinyint(1)      default 1            not null,
+    start_time        time                                 not null,
+    duration          int                                  not null,
+    recurrence_type   text                                 null,
+    recurrence_period text                                 null,
+    recurrence_start  date                                 null,
+    recurrence_end    date                                 null,
     constraint times_shows
         foreign key (`show`) references shows (id)
 );
 
 create table urls
 (
-    id   bigint unsigned default(uuid_short())
+    id   bigint unsigned default uuid_short() not null
         primary key,
-    name text not null,
-    url  text not null
+    name text                                 not null,
+    url  text                                 not null
 );
 
 create table rel_urls
 (
-    id      bigint unsigned default(uuid_short())
+    id      bigint unsigned default uuid_short() not null
         primary key,
-    url     bigint unsigned not null,
-    `show`  bigint unsigned null,
-    sponsor bigint unsigned null,
-    people  bigint unsigned null,
+    url     bigint unsigned                      not null,
+    `show`  bigint unsigned                      null,
+    sponsor bigint unsigned                      null,
+    people  bigint unsigned                      null,
     constraint rel_urls_people
         foreign key (people) references people (id),
     constraint rel_urls_shows
@@ -120,5 +122,4 @@ create table rel_urls
     constraint rel_urls_url
         foreign key (url) references urls (id)
 );
-
 
